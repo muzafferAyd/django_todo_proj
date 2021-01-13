f61e335c1adfe70a8cc1c46865616c08189a6d92
Muzaffer <aydogdum38@gmail.com>
2021-01-13 22:42:37 +0300

    added  todo-create and todo-update

--- src/todo/models.py
+++ src/todo/models.py
@@ -5,6 +5,10 @@ class Todo(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ('-created_date',)
+
+
     def __str__(self):
         return self.title
 
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
+from .forms import TodoAddForm, TodoUpdateForm
 
 def home(request):
     return render(request, "todo/home.html")
@@ -11,3 +12,30 @@ def todo_list(request):
         'todos' : todos
     }
     return render(request, "todo/todo_list.html", context)
+
+def todo_create(request):
+    form = TodoAddForm()
+    if request.method == "POST":
+        form = TodoAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("house")
+    context = {
+        'form' : form
+    }
+    return render(request, "todo/todo_create.html", context)
+
+def todo_update(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    form = TodoUpdateForm(instance=todo)
+    if (request.method == "POST"):
+        form = TodoUpdateForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("todo-list")
+    context = {
+        'todo' : todo,
+        'form' : form
+    }
+    return render(request, "todo/todo_update.html", context )
+
